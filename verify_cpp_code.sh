821d00c5643f74672a1dac7b59629262d7814dbc
Victor Marinsky <victor.marinsky@gmail.com>
2017-01-07 14:55:57 -0500

    Add configuration and launcher for style analyzer

---
+++ verify_cpp_code.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This script is a wrapper on code verification which currently includes
+# code style analysis only.
+
+# set working directory to script directory
+cd "$(dirname "$0")"
+# if no arguments passed, then scan the following folders (all sources)
+SCAN_PATH=$(echo ./{src/data_providers,src/business_logic,test})
+SRC_EXTS='( -name *.h -o -name *.cpp -o -name *.hpp )'
+SCAN_FILES="$(find $SCAN_PATH -type f $SRC_EXTS)"
+
+# otherwise scan specific files/folders specified as arguments
+if [ ! -z "$1" ]; then
+    SCAN_FILES="$*"
+fi
+
+# run verification script with extra settings
+# google's cpplint requires python2, can be easily ported to python3 if needed
+python2 3rdparty/styleguide/cpplint/cpplint.py \
+    --verbose=0 \
+    --counting=toplevel \
+    $SCAN_FILES
